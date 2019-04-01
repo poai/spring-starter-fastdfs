8ea6e550f32b5ea6733260d8c111a118e46b33b1
xiaofen <xiaofen@XiaoFen>
2019-04-01 17:24:29 +0800

    扩展文件后缀

--- src/main/java/com/zjf/fastdfs/FDFSUtil.java
+++ src/main/java/com/zjf/fastdfs/FDFSUtil.java
@@ -9,11 +9,25 @@ import org.csource.fastdfs.StorageClient;
 public class FDFSUtil {
 	private static Log log = LogFactory.getLog(FDFSUtil.class);
 	private static ThreadLocal<StorageClient> threadLocal = new ThreadLocal<>();
+	private static final String DEFAULT_FILE_EXT_NAME = "jpg";
 
 	public String[] upload(byte[] image) {
 		try {
 			long start = System.currentTimeMillis();
-			String values[] = getClient().upload_file(image, ".jpg", null);
+			String values[] = getClient().upload_file(image, DEFAULT_FILE_EXT_NAME, null);
+			long end = System.currentTimeMillis();
+			log.debug("UPLOAD FILE SUCCESS USE TIME:" + (end - start) + ",RESULT:" + Arrays.asList(values));
+			return values;
+		} catch (Exception e) {
+			log.error(e);
+			return null;
+		}
+	}
+
+	public String[] upload(byte[] image, String fileExtName) {
+		try {
+			long start = System.currentTimeMillis();
+			String values[] = getClient().upload_file(image, fileExtName, null);
 			long end = System.currentTimeMillis();
 			log.debug("UPLOAD FILE SUCCESS USE TIME:" + (end - start) + ",RESULT:" + Arrays.asList(values));
 			return values;
